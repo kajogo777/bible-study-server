5b8a025bb5ed646a5bdf5512c72651ddb6d78602
george <kajogo777@gmail.com>
2019-08-16 19:09:42 +0200

    Persist db

--- src/ch_app_server/settings.py
+++ src/ch_app_server/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'ch_app_server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db/db.sqlite3'),
     }
 }
 
