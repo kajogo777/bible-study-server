5bb63fbb257c2a535681432673575b7bb693110a
george <kajogo777@gmail.com>
2019-09-14 01:46:19 +0200

    Nits

--- src/users/admin.py
+++ src/users/admin.py
@@ -30,67 +30,110 @@ class AdminUserAdmin(UserAdmin):
 
 
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ('name', 'downloads')
+    list_display = ('name',)  # 'downloads')
 
-    def get_urls(self):
-        urls = super(GroupAdmin, self).get_urls()
-        urls += [
-            url(r'^download-file/(?P<pk>\d+)$', self.download_pdf,
-                name='applabel_modelname_download-file'),
-        ]
-        return urls
+    # def get_urls(self):
+    #     urls = super(GroupAdmin, self).get_urls()
+    #     urls += [
+    #         url(r'^download-file/(?P<pk>\d+)$', self.download_pdf,
+    #             name='applabel_modelname_download-file'),
+    #     ]
+    #     return urls
 
-    def downloads(self, obj):
-        return format_html(
-            '<a href="{}">Download QR codes</a>',
-            reverse('admin:applabel_modelname_download-file', args=[obj.pk])
-        )
-    downloads.short_description = "Downloads"
+    # def downloads(self, obj):
+    #     return format_html(
+    #         '<a href="{}">Download QR codes</a>',
+    #         reverse('admin:applabel_modelname_download-file', args=[obj.pk])
+    #     )
+    # downloads.short_description = "Downloads"
 
-    def download_pdf(self, request, pk):
-        if request.user.is_authenticated:
-            response = HttpResponse(content_type='application/pdf')
-            response['Content-Disposition'] = 'attachment; filename=group_qr_codes.pdf'
-            p = canvas.Canvas(response)
+    # def download_pdf(self, request, pk):
+    #     if request.user.is_authenticated:
+    #         response = HttpResponse(content_type='application/pdf')
+    #         response['Content-Disposition'] = 'attachment; filename=group_qr_codes.pdf'
+    #         p = canvas.Canvas(response)
 
-            users = User.objects.filter(group__id=pk)
+    #         users = User.objects.filter(group__id=pk)
 
-            per_page = 6
-            x_offset = 0
-            y_offset = 0
-            step = 130
-            margin = 50
+    #         per_page = 6
+    #         x_offset = 0
+    #         y_offset = 0
+    #         step = 130
+    #         margin = 50
 
-            for i, user in enumerate(users):
-                qrw = QrCodeWidget(user.code)
+    #         for i, user in enumerate(users):
+    #             qrw = QrCodeWidget(user.code)
 
-                d = Drawing(50, 50)
-                d.add(qrw)
-                renderPDF.draw(d, p, margin + x_offset, margin + y_offset)
-                p.drawString(margin + x_offset, margin +
-                             y_offset - 10, user.name)
+    #             d = Drawing(50, 50)
+    #             d.add(qrw)
+    #             renderPDF.draw(d, p, margin + x_offset, margin + y_offset)
+    #             p.drawString(margin + x_offset, margin +
+    #                          y_offset - 10, user.name)
 
-                y_offset += step
+    #             y_offset += step
 
-                if i % per_page == per_page - 1:
-                    x_offset += step + 60
-                    y_offset = 0
+    #             if i % per_page == per_page - 1:
+    #                 x_offset += step + 60
+    #                 y_offset = 0
 
-                if i % (per_page*3) == (per_page*3) - 1:
-                    p.showPage()
-                    x_offset = 0
-                    y_offset = 0
+    #             if i % (per_page*3) == (per_page*3) - 1:
+    #                 p.showPage()
+    #                 x_offset = 0
+    #                 y_offset = 0
 
-            p.save()
-            return response
-        else:
-            return HttpResponse('Unauthorized', status=403)
+    #         p.save()
+    #         return response
+    #     else:
+    #         return HttpResponse('Unauthorized', status=403)
 
 
 class ResponseInline(admin.TabularInline):
     model = Response
     extra = 0
     min_num = 0
+
+
+def download_pdf(modeladmin, request, queryset):
+    if request.user.is_authenticated:
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename=group_qr_codes.pdf'
+        p = canvas.Canvas(response)
+
+        users = queryset  # User.objects.filter(group__id=pk)
+
+        per_page = 6
+        x_offset = 0
+        y_offset = 0
+        step = 130
+        margin = 50
+
+        for i, user in enumerate(users):
+            qrw = QrCodeWidget(user.code)
+
+            d = Drawing(50, 50)
+            d.add(qrw)
+            renderPDF.draw(d, p, margin + x_offset, margin + y_offset)
+            p.drawString(margin + x_offset, margin +
+                         y_offset - 10, user.name)
+
+            y_offset += step
+
+            if i % per_page == per_page - 1:
+                x_offset += step + 60
+                y_offset = 0
+
+            if i % (per_page*3) == (per_page*3) - 1:
+                p.showPage()
+                x_offset = 0
+                y_offset = 0
+
+        p.save()
+        return response
+    else:
+        return HttpResponse('Unauthorized', status=403)
+
+
+download_pdf.short_description = "Download QR codes"
 
 
 class RegularUserForm(forms.ModelForm):
@@ -102,13 +145,15 @@ class RegularUserForm(forms.ModelForm):
 
 
 class RegularUserAdmin(admin.ModelAdmin):
-    list_display = ('name', 'date_of_birth', 'gender', 'code')
+    list_display = ('name', 'date_of_birth', 'gender', 'group', 'code')
     list_filter = (
         'date_of_birth',
+        'group'
     )
     inlines = [
         ResponseInline,
     ]
+    actions = [download_pdf]
     form = RegularUserForm
 
     def get_form(self, request, *args, **kwargs):
