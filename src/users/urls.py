06db60da4a7bad7b4e18a9d8f74007e2d0c3291e
george sedky <kajogo777@gmail.com>
2020-04-12 12:24:19 +0200

    Add user score endpoint

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,7 +1,8 @@
 from rest_framework import routers
-from .views import UsersViewSet, ResponseViewSet, GroupsViewSet
+from .views import UsersViewSet, ResponseViewSet, GroupsViewSet, UserScoreViewSet
 
 router = routers.SimpleRouter()
 router.register(r'users', UsersViewSet, basename='user')
 router.register(r'groups', GroupsViewSet)
 router.register(r'responses', ResponseViewSet)
+router.register(r'score', UserScoreViewSet, basename='score')
--- src/users/views.py
+++ src/users/views.py
@@ -1,9 +1,14 @@
-from rest_framework import viewsets, serializers
+from rest_framework import viewsets, serializers, mixins
 from django_filters import rest_framework as filters
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response as HttpResponse
+from challenges.models import Challenge
 from .models import User, Group, Response
 from .authentication import CodeAuthentication, IsOwner, IsAuthenticated
 from django.utils import timezone
+from datetime import timedelta
+from django.db.models import Sum
+from django.core.cache import cache
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -60,3 +65,61 @@ class ResponseViewSet(viewsets.ModelViewSet):
     serializer_class = ResponseSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_fields = ('challenge',)
+
+
+class UserScoreViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    authentication_classes = (CodeAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def list(self, request, format=None):
+        user = request.user
+        today = timezone.localtime(timezone.now()).date()
+        last_30_days = today - timedelta(days=30)
+
+        user_challenges = Challenge.objects.filter(
+            group=user.group,
+            active_date__lt=today
+        )
+        user_challenges_last_30_days = user_challenges.filter(
+            active_date__gt=last_30_days
+        )
+        user_attempted_responses = Response.objects.filter(
+            user=user,
+        )
+        user_attempted_responses_last_30_days = user_attempted_responses.filter(
+            challenge__active_date__gt=last_30_days
+        )
+        user_correct_responses = user_attempted_responses.filter(
+            answer__correct=True
+        )
+        user_correct_responses_last_30_days = user_correct_responses.filter(
+            challenge__active_date__gt=last_30_days
+        )
+
+        total_challenges = user_challenges.count()
+        total_challenges_last_30_days = user_challenges_last_30_days.count()
+
+        total_correct = user_correct_responses.count()
+        total_correct_last_30_days = user_correct_responses_last_30_days.count()
+
+        total_attempted = user_attempted_responses.count()
+        total_attempted_last_30_days = user_attempted_responses_last_30_days.count()
+
+        total_score = user_correct_responses.aggregate(
+            Sum('challenge__reward_score')
+        ).get('challenge__reward_score__sum', 0)
+        total_score_last_30_days = user_correct_responses_last_30_days.aggregate(
+            Sum('challenge__reward_score')
+        ).get('challenge__reward_score__sum', 0)
+
+        return HttpResponse({
+            'total_challenges': total_challenges,
+            'total_attempted': total_attempted,
+            'total_correct': total_correct,
+            'total_score': total_score,
+
+            'total_challenges_last_30_days': total_challenges_last_30_days,
+            'total_attempted_last_30_days': total_attempted_last_30_days,
+            'total_correct_last_30_days': total_correct_last_30_days,
+            'total_score_last_30_days': total_score_last_30_days,
+        })
