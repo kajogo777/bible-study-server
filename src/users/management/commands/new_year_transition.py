551b7254c0a5b112370c9eb2512680c783d151b3
George Sedky <george@devopzilla.com>
2022-09-30 00:15:59 +0200

    Update transition script & missing migration

--- src/users/management/commands/new_year_transition.py
+++ src/users/management/commands/new_year_transition.py
@@ -1,57 +1,49 @@
-from django.core.management.base import BaseCommand, CommandError
-from users.models import Group, Class, User
-
-
-MAIN_GROUPS = ["Primary", "Preparatory", "Secondary"]
+from django.core.management.base import BaseCommand
+from django.db.models import F
+from users.models import Group, User
 
 
 class Command(BaseCommand):
-    help = "Transition users at the end of each group class to the next group"
+    help = "Transition users by 1 grade and move them to new groups"
 
     def handle(self, *args, **options):
-        groups = []
+        primary_group = Group.objects.get(name="Primary")
+        preparatory_group = Group.objects.get(name="Preparatory")
+        secondary_group = Group.objects.get(name="Secondary")
+        university_group = Group.objects.get(name="University")
 
-        for g in MAIN_GROUPS:
-            groups.append(Group.objects.get(name=g))
 
-        groups_max_grade = []
-
-        for i in range(len(MAIN_GROUPS)):
-            try:
-                groups_max_grade.append(
-                    Class.objects.filter(group=groups[i])
-                    .order_by("-grade")
-                    .first()
-                    .grade
-                )
-            except Exception:
-                raise CommandError(f"Group {groups[i]} does not have classes")
-
-        trans = {}
-
-        trans[groups[0].id] = groups[1]
-        trans[groups[1].id] = groups[2]
-        trans[groups[2].id], _ = Group.objects.get_or_create(name="University")
-
-        for i in range(len(MAIN_GROUPS)):
-            print(groups[i])
-            print(trans[groups[i].id])
-            result = User.objects.filter(
-                group=groups[i], group_class__grade=groups_max_grade[i]
-            ).update(group=trans[groups[i].id], group_class=None)
-
-            self.stdout.write(
-                self.style.SUCCESS(
-                    f"Successfully transitioned {result} users from group {groups[i]} to group {trans[groups[i].id]}"
-                )
+        result = User.objects.filter(grade__lt=13).update(grade=F('grade')+1)
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Successfully incremented grade of {result} users"
             )
+        )
 
-            result = User.objects.filter(
-                group=groups[i], group_class__grade__lt=groups_max_grade[i]
-            ).update(group_class=None)
-
-            self.stdout.write(
-                self.style.SUCCESS(
-                    f"Successfully removed {result} users from classes in group {groups[i]}"
-                )
+        result = User.objects.filter(grade__gte=1, grade__lte=6).update(group=primary_group)
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Successfully set {result} users Primary"
             )
+        )
+
+        result = User.objects.filter(grade__gte=7, grade__lte=9).update(group=preparatory_group)
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Successfully set {result} users Preparatory"
+            )
+        )
+
+        result = User.objects.filter(grade__gte=10, grade__lte=12).update(group=secondary_group)
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Successfully set {result} users Secondary"
+            )
+        )
+
+        result = User.objects.filter(grade=13).update(group=university_group)
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Successfully set {result} users University"
+            )
+        )
